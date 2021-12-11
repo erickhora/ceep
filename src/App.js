f562d5841abf95610fc1649db9e9253620662e5d
Erick Hora <erickn.hora@gmail.com>
2021-12-11 13:35:30 -0300

    - Added state management to make notes dynamic.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas";
 import "./assets/App.css";
+import "./assets/index.css";
 
-function App() {
-	return (
-		<section className="conteudo">
-			<FormularioCadastro />
-			<ListaDeNotas />
-		</section>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			notas: [],
+		};
+	}
+
+	criarNota(titulo, texto) {
+		const novaNota = { titulo, texto };
+		const novoArrayNotas = [...this.state.notas, novaNota];
+		const novoEstado = {
+			notas: novoArrayNotas,
+		};
+		this.setState(novoEstado);
+	}
+	render() {
+		return (
+			<section className="conteudo">
+				<FormularioCadastro criarNota={this.criarNota.bind(this)} />
+				<ListaDeNotas notas={this.state.notas} />
+			</section>
+		);
+	}
 }
 
 export default App;
